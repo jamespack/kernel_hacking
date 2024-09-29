789819a5635aa673fef94762742ac9561af4f247
James Pack <james.packjr@outlook.com>
2024-09-29 11:23:10 -0400

    Minimal working kernel hacking env

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ pushd output/ramdisk/
 for f in "$@"; do
 	cp $DIR/"$@" .
 done
+mkdir -p bin sbin etc proc sys usr/bin usr/sbin
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initramfs.cpio.gz
 popd
 
-qemu-system-x86_64 -m 1G -kernel output/linux/arch/x86_64/boot/bzImage -nographic -append 'console=ttyS0 loglevel=7 raid=noautodetect initcall_debug debug rw rootfstype=ramfs' -enable-kvm -initrd output/initramfs.cpio.gz
+qemu-system-x86_64 -m 1G -kernel output/linux/arch/x86_64/boot/bzImage -nographic -append 'console=ttyS0 loglevel=15' -enable-kvm -initrd output/initramfs.cpio.gz
