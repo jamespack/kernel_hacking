ccc2b1ddac119b6ebf61551b1fab7ec5bae822aa
James Pack <james.packjr@outlook.com>
2024-09-30 13:28:00 -0400

    Update run script to copy tools subfolders

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# copy tools to ramdisk
+
+pushd tools
+find . -mindepth 1 -maxdepth 1 -print -exec cp -r '{}' ../output/ramdisk \;
+popd
+
 DIR="$(realpath $(dirname "$0"))"
 
 pushd output/ramdisk/
@@ -7,12 +13,10 @@ for f in "$@"; do
 	cp $DIR/"$@" .
 done
 
-# copy tools to ramdisk
-
-find tools/ -type d -exec cp -r '{}' output/ramdisk \
-
 mkdir -p bin sbin etc proc sys usr/bin usr/sbin
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initramfs.cpio.gz
 popd
+
+
 
 qemu-system-x86_64 -m 1G -kernel output/linux/arch/x86_64/boot/bzImage -nographic -net nic  -net user -cpu host -append 'console=ttyS0 loglevel=15' -enable-kvm -initrd output/initramfs.cpio.gz
